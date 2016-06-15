f5a58ed2f7bdcccd129a6d88ad617bb62b9218b9
Mateusz Zajdel <mateusz.zajdel@outlook.com>
2016-06-15 16:34:56 +0200

    Początkowa część kodu. W funkcji przesluchanie nie działają poprawnie ify, dziwna sprawa - nie wiem dlaczego. Rozwiązanie pewnie jest trywialne, jednak póki co go nie widzę.

--- dylemat_wieznia_app/main.cpp
+++ dylemat_wieznia_app/main.cpp
@@ -1,12 +1,129 @@
 #include <iostream>
+#include <vector>
+#include <ctime> 
+#include <cstdlib>
+
+
+//#define WSPOLPRACA 0
+//#define ZDRADA 1
+
+#define LICZBA_WIEZNIOW 3
+int licznik = 0; //zmienna kontrolna
 
 using namespace std;
 
-bool main() {
-	cout << "hello world!";
-	//hello2
-  // hello3
+class Osobnik
+{
+public:
+	bool chromosom[64];
+	bool poprzednie[6];
+	unsigned int wyrok;
+	Osobnik()
+	{
+		wyrok = 0;
+	}
+
+	void inicjalizuj()
+	{
+		for (int i = 0; i < 64; i++)
+			chromosom[i] = rand() % 2;
+		for (int i = 0; i < 6; i++)
+			poprzednie[i] = rand() % 2;
+	}
+	void wyswietl_chromosom()
+	{
+		for (int i = 0; i < 64; i++)
+		{
+			cout << chromosom[i];
+		}
+		cout << endl << endl;
+	}
+};
+
+void inicjalizuj_populacje(vector <Osobnik> &populacja)
+{
+	Osobnik *wsk;
+	srand(time(NULL));
+	for (int i = 0; i < LICZBA_WIEZNIOW; i++)
+	{
+		wsk = new Osobnik;
+		populacja.push_back(*wsk);
+		wsk->inicjalizuj();
+	}
+}
+
+int bin_to_dec(bool *bin)
+{
+	int dec = 0;
+	int j = 0;
+	for (int i = 5; i >=0; i--)
+	{
+		if (bin[i] == true)
+			dec += pow(2, j);
+		j++;
+	}
+	return dec;
+}
+
+void przesluchanie(Osobnik &A, Osobnik &B)
+{
+	licznik++;
+	cout << "przesluchanie nr " << licznik << endl;
+	bool a = A.chromosom[bin_to_dec(B.poprzednie)]; //wiêzieñ A podejmuje decyzjê w oparciu o strategie wiêŸnia B w poprzednich przes³uchaniach
+	bool b = B.chromosom[bin_to_dec(A.poprzednie)]; //wiêzieñ B analogicznie
+	
+	if (a == false && b == false)
+	{
+		A.wyrok += 2;
+		B.wyrok += 2;
+	}
+	else if (a == false && b == true)
+	{
+		A.wyrok += 10;
+	}
+	else if (a == true && b == false)
+	{
+		B.wyrok += 10;
+	}
+	else if (a == true && b == true)
+	{
+		A.wyrok += 10;
+		B.wyrok += 10;
+	}
+	else 
+		cout << "BLAD!" << endl;
+	
+	A.poprzednie[0] = A.poprzednie[2];
+	A.poprzednie[1] = A.poprzednie[3];
+	A.poprzednie[2] = A.poprzednie[4];
+	A.poprzednie[3] = A.poprzednie[5];
+	A.poprzednie[4] = a;	//decyzja podjêta w aktualnym przes³uchaniu przez wiêŸnia A
+	A.poprzednie[5] = b;	//decyzja podjêta w aktualnym przes³uchaniu przez wiêŸnia B
+
+	B.poprzednie[0] = B.poprzednie[2];
+	B.poprzednie[1] = B.poprzednie[3];
+	B.poprzednie[2] = B.poprzednie[4];
+	B.poprzednie[3] = B.poprzednie[5];
+	B.poprzednie[4] = b;
+	B.poprzednie[5] = a;
+	
+}
 
 
+
+bool main() 
+{
+	vector <Osobnik> populacja;	//wektor przechowuj¹cy populacjê - wszystkich wiêŸniów
+	inicjalizuj_populacje(populacja);
+	
+	for (int i = LICZBA_WIEZNIOW - 1; i >= 0; i--)
+	{
+		for (int j = i - 1; j >= 0; j--)
+		{
+			przesluchanie(populacja[i], populacja[j]);
+		}
+	}
+
+	system("pause");
 	return 0;
 }
